c1a66fad8bc460a7b336a9d7bdffa8f0e132e272
Bobby Bearden <brbearden@gmail.com>
2014-09-23 11:59:05 -0600

    Complete development of panel

--- src/pwrtriangle/Panel.java
+++ src/pwrtriangle/Panel.java
@@ -6,17 +6,19 @@ import javax.swing.*;
 /**
  * Main panel for the Power Triangle Solver
  *
- * This panel is the main component in the root frame of the application. This
- * panel contains sub-panels to choose the computational mode and enter
- * parameters and display results.
+ * This panel is the main component in the root frame of the application. The
+ * user uses this panel to set the computation mode and enter inputs. The
+ * results are then displayed on this panel as well.
  */
 public class Panel extends Box implements ActionListener {
 
-    static private final int NUM_MODES = 3;
     static private final int PQ_MODE = 0;
     static private final int PPF_MODE = 1;
     static private final int QPF_MODE = 2;
-    
+
+    static private final String OP1_CMD = "op1";
+    static private final String OP2_CMD = "op2";
+
     private final String[] MODES;
     private final String[] OP1_LABELS;
     private final String[] OP1_UNITS;
@@ -25,12 +27,11 @@ public class Panel extends Box implements ActionListener {
     private final String[] RESULT_LABELS;
     private final String[] RESULT_UNITS;
 
-    
     private JComboBox mode;
     private JTextField operandOne;
     private JTextField operandTwo;
     private JLabel result;
-    
+
     private JLabel operandOneLabel;
     private JLabel operandOneUnits;
     private JLabel operandTwoLabel;
@@ -40,11 +41,14 @@ public class Panel extends Box implements ActionListener {
 
     /**
      * Default Constructor
+     *
+     * This panel is a
      */
     public Panel() {
 
         super(BoxLayout.Y_AXIS);
-        
+
+        // Initialize label strings
         MODES = new String[]{"PQ", "PPF", "QPF"};
         OP1_LABELS = new String[]{"Active Power", "Active Power", "Reactive Power"};
         OP1_UNITS = new String[]{"W", "W", "VAr"};
@@ -53,42 +57,107 @@ public class Panel extends Box implements ActionListener {
         RESULT_LABELS = new String[]{"Power Factor", "Reactive Power", "Active Power"};
         RESULT_UNITS = new String[]{"", "VAr", "W"};
 
+        // Create mode selection
         mode = new JComboBox(MODES);
-        mode.addActionListener(this);
         mode.setSelectedIndex(PQ_MODE);
-        add(mode);
-        
-        Box opOneRow = Box.createHorizontalBox();
-        
+        mode.addActionListener(this);
+
+        operandOneLabel = new JLabel("");
         operandOne = new JTextField("0");
+        operandOne.setActionCommand(OP1_CMD);
         operandOne.addActionListener(this);
-        
+        operandOneUnits = new JLabel("");
+
+        Box opOneRow = Box.createHorizontalBox();
         opOneRow.add(operandOneLabel);
         opOneRow.add(operandOne);
         opOneRow.add(operandOneUnits);
-        add(opOneRow);
+
+        operandTwoLabel = new JLabel("");
+        operandTwo = new JTextField("0");
+        operandTwo.setActionCommand(OP2_CMD);
+        operandTwo.addActionListener(this);
+        operandTwoUnits = new JLabel("");
 
         Box opTwoRow = Box.createHorizontalBox();
-        
-        operandTwo = new JTextField("0");
-        operandTwo.addActionListener(this);
-        
         opTwoRow.add(operandTwoLabel);
         opTwoRow.add(operandTwo);
         opTwoRow.add(operandTwoUnits);
-        add(opTwoRow);
-        
-        Box resultRow = Box.createHorizontalBox();
-        result = new JLabel("0");
 
+        resultLabel = new JLabel("");
+        result = new JLabel("0");
+        resultUnits = new JLabel("");
+
+        Box resultRow = Box.createHorizontalBox();
+        resultRow.add(resultLabel);
+        resultRow.add(result);
+        resultRow.add(resultUnits);
+
+        add(mode);
+        add(opOneRow);
+        add(opTwoRow);
+        add(resultRow);
+
+        updatePanelMode(mode.getSelectedIndex());
     }
 
     /**
-     * Handle the actions associated with this panel
+     * Handle the actions associated with this panel.
      * @param e Action event data
      */
     @Override
     public void actionPerformed(ActionEvent e) {
+
+        if(e.getActionCommand().equals(mode.getActionCommand()))
+            updatePanelMode(mode.getSelectedIndex());
+        else if(e.getActionCommand().equals(OP1_CMD) || e.getActionCommand().equals(OP2_CMD))
+            calculateResult(mode.getSelectedIndex());
+    }
+
+    /**
+     * Update panel in response to mode changes.
+     * @param newMode new computational mode
+     */
+    private void updatePanelMode(int newMode) {
+
+        // Update mode labels if mode is valid
+        if((newMode < MODES.length) && (newMode >= 0)) {
+
+            operandOneLabel.setText(OP1_LABELS[newMode]);
+            operandOneUnits.setText(OP1_UNITS[newMode]);
+
+            operandTwoLabel.setText(OP2_LABELS[newMode]);
+            operandTwoUnits.setText(OP2_UNITS[newMode]);
+
+            resultLabel.setText(RESULT_LABELS[newMode]);
+            resultUnits.setText(RESULT_UNITS[newMode]);
+        }
+
+        // Clear contents
+        operandOne.setText("0");
+        operandTwo.setText("0");
+        result.setText("0");
+    }
+
+    /**
+     * Calculate new computationResult and update the computationResult field.
+     * @param mode computational mode
+     */
+    private void calculateResult(int mode) {
+
+        double computationResult = 0;
+        double op1 = Double.parseDouble(operandOne.getText());
+        double op2 = Double.parseDouble(operandTwo.getText());
+
+        if(mode == PQ_MODE) {
+            computationResult = Solver.solveForPf(op1, op2);
+        } else if(mode == PPF_MODE) {
+            computationResult = Solver.solveForQ(op1, op2);
+        } else if(mode == QPF_MODE) {
+            computationResult = Solver.solveForP(op1, op2);
+        }
+
+        this.result.setText(String.valueOf(computationResult));
     }
 
 }
